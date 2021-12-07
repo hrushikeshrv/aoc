55e984a5bb75cf05d4c9141f7f73529a06232ac4
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2021-12-07 21:27:39 +0530

    Added day 7 part 2 2021

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,10 +1,21 @@
 """
+Problem 7 - https://adventofcode.com/2021/day/6
 
+Part 1 -
+    Given a list of numbers L, find a number m such that the sum of absolute differences between m and L[i]
+    is minimum
+
+Part 2 -
+    Given the same list of numbers L, find a number m such that the sum of |L[i] - m| * (|L[i] - m| + 1) / 2 for all i
+    is minimum
 """
 
 # Set up the input
 with open('input-07.txt', 'r') as file:
     pos = sorted(list(map(lambda x: int(x), file.readline().split(','))))
+
+with open('input-07-test.txt', 'r') as file:
+    test_pos = sorted(list(map(lambda x: int(x), file.readline().split(','))))
 
 
 # Solution to part 1
@@ -23,3 +34,43 @@ def solve_1(positions):
 ans = solve_1(pos)
 print(ans)
 # Answer was 348664
+
+
+# Define helper functions
+def fuel_cost(x1, x2):
+    """
+    Returns the amount of fuel needed to move a crab submarine from x1 to x2
+    """
+    d = abs(x1 - x2)
+    return (d*(d+1))/2
+
+
+def weighted_average(numbers):
+    """
+    Returns the weighted average of the numbers array weighted by the frequency of the numbers
+    :param numbers:List[int]
+    :return: float
+    """
+    num_dict = {}
+    for n in numbers:
+        num_dict[n] = num_dict.get(n, 0) + 1
+    weighted_sum = 0
+    for n in num_dict:
+        weighted_sum += n * num_dict[n]
+    return weighted_sum/len(numbers)
+
+
+# Solution to part 2
+def solve_2(positions):
+    w_a = weighted_average(positions)
+    w_a = int(w_a)      # Take the floor of the number if it is a float
+    
+    fuel = 0
+    for p in positions:
+        fuel += fuel_cost(p, w_a)
+    return fuel
+
+
+ans = solve_2(pos)
+print(ans)
+# Answer was 100220525
