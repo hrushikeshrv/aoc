af62cb63bbaa4494eed5f6e40ebfba9dd1e6bfad
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2021-12-15 20:40:56 +0530

    Added part 1 day 15

--- 2021/day15.py
+++ 2021/day15.py
@@ -7,7 +7,6 @@ with open('input-15.txt', 'r') as file:
     vents = list(map(lambda x: x.strip(), file.readlines()))
     max_x_1 = len(vents[0]) - 1
     max_y_1 = len(vents) - 1
-    print(max_x_1, max_y_1)
     cache_1 = [[None for x in range(max_x_1 + 1)] for y in range(max_y_1 + 1)]
 
 with open('input-15-test.txt', 'r') as file:
@@ -18,29 +17,29 @@ with open('input-15-test.txt', 'r') as file:
 
 
 # Define helper functions
-def shortest_path(x, y, max_x, max_y, cache, entering, vent_map):
-    if x == max_x - 1 and y == max_y - 1:
+def shortest_path(x, y, max_x, max_y, cache, vent_map):
+    if x == max_x and y == max_y:
+        # Base case
+        cache[y][x] = int(vent_map[y][x])
         return int(vent_map[y][x])
     if cache[y][x] is not None:
         return cache[y][x]
-    paths = []
-    for new_x, new_y in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]:
-        if (new_x, new_y) == entering:
-            continue
+    min_risk = 999999999
+    for new_x, new_y in [(x+1, y), (x, y+1)]:
         if not 0 <= new_x <= max_x or not 0 <= new_y <= max_y:
             continue
-        if cache[new_y][new_x] is not None:
-            return cache[new_y][new_x]
-        print(f'Finding shortest path from {x, y}. Considering neighbour {new_x, new_y}')
-        paths.append(shortest_path(new_x, new_y, max_x, max_y, cache, entering, vent_map) + int(vent_map[new_y][new_x]))
-    cache[y][x] = min(paths)
-    return min(paths)
+        _ = shortest_path(new_x, new_y, max_x, max_y, cache, vent_map) + int(vent_map[new_y][new_x])
+        if _ < min_risk:
+            min_risk = _
+    cache[y][x] = min_risk
+    return min_risk
 
 
 # Solution to part 1
 def solve_1(vent_map, max_x, max_y, cache):
-    return shortest_path(0, 0, max_x, max_y, cache, (None, None), vent_map)
+    return shortest_path(0, 0, max_x, max_y, cache, vent_map) - int(vent_map[0][0])
+    
 
-
-ans = solve_1(test_vents, test_max_x, test_max_y, test_cache)
+ans = solve_1(vents, max_x_1, max_y_1, cache_1)
 print(ans)
+# Answer was 626
