00b3315cb7119acfe5536ae2dd5a3a78a76b0502
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2021-12-17 12:04:17 +0530

    Approach 2 for day 17 part 1

--- 2021/day17.py
+++ 2021/day17.py
@@ -3,32 +3,38 @@ Problem 17
 """
 
 x_range = range(277, 319)
-y_range = range(52, 92)  # Not taking negatives for the first part
+y_range = range(53, 93)  # Not taking negatives for the first part
 
 test_x_range = range(20, 31)
-test_y_range = range(4, 10)
+test_y_range = range(5, 11)
 
 
 # Solution to part 1
 def solve_1(target_y_range, max_y):
     vy = 0
-    found_max_vel = False
-    while True:
-        if found_max_vel:
-            break
+    max_vel = 0
+    candidates = []
+    while vy <= max_y:
         vy += 1
         n = 1
         y = n * vy + n * (n - 1) / 2
-        # print(f'y currently for vy = {vy} and n = {n} is {y}')
-        while y not in target_y_range:
+        while True:
             if y > max_y:
-                found_max_vel = True
                 break
+            if y in target_y_range:
+                max_vel = vy
+                candidates.append((max_vel, n))
+                print(f'Found a velocity - {vy}. y is {y} at time {n}')
             n += 1
             y = n * vy + n * (n - 1) / 2
-    vy -= 1
-    return vy * (vy + 1) / 2
+    while candidates:
+        vel, n = candidates.pop()
+        # Find the correct value of n
+        # Iterate over all x velocities from 1 to max_x
+        # If we find a matching x velocity which results in x coord being in x_range, this y velocity is correct
+        
+    return max_vel * (max_vel + 1) / 2
 
 
-ans = solve_1(y_range, 92)
+ans = solve_1(test_y_range, 10)
 print(ans)
