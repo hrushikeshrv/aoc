3b5ad230fb275f164a87c1bb89dcc8c469794761
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-01 10:38:57 +0530

    Added 2022 day1

---
+++ 2022/day01.py
@@ -0,0 +1,35 @@
+# Set up the input
+with open('input-01.txt.', 'r') as file:
+    lines = list(map(lambda x: x.strip(), file.readlines()))
+
+def solve_1(cals):
+    max_cals = 0
+    _ = 0
+    for line in cals:
+        if not line:
+            if _ > max_cals:
+                max_cals = _
+            _ = 0
+        else:
+            _ += int(line)
+    return max_cals
+
+ans = solve_1(lines)
+print(ans)
+# Answer was 69177
+
+def solve_2(cals):
+    cals_sorted = []
+    _ = 0
+    for line in cals:
+        if not line:
+            cals_sorted.append(_)
+            _ = 0
+        else:
+            _ += int(line)
+    cals_sorted.sort()
+    return sum(cals_sorted[-3:])
+
+ans = solve_2(lines)
+print(ans)
+# Answer was 207456
