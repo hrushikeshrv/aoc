0fff78da1eac9cc3ef7c56bae8953ebf94f8d437
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-06 10:46:25 +0530

    Added 2022 day 6

---
+++ 2022/day06.py
@@ -0,0 +1,19 @@
+with open('input-06.txt', 'r') as file:
+    line = file.read().strip()
+
+
+def solve1(line):
+    for i in range(4, len(line)):
+        if len(set(line[i-4:i])) == 4:
+            return i
+
+ans = solve1(line)
+print(ans)
+
+def solve2(line):
+    for i in range(14, len(line)):
+        if len(set(line[i-14:i])) == 14:
+            return i
+
+ans = solve2(line)
+print(ans)
