04a26e3b3736c7dd2975e744736dec6fbcc91dfe
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-07 19:10:29 +0530

    Added 2022 day 7

---
+++ 2022/day07.py
@@ -0,0 +1,93 @@
+with open('input-07.txt', 'r') as file:
+    commands = list(map(lambda x: x.strip(), file.readlines()))
+
+# commands = [
+#     '$ cd /',
+#     '$ ls',
+#     'dir a',
+#     '14848514 b.txt',
+#     '8504156 c.dat',
+#     'dir d',
+#     '$ cd a',
+#     '$ ls',
+#     'dir e',
+#     '29116 f',
+#     '2557 g',
+#     '62596 h.lst',
+#     '$ cd e',
+#     '$ ls',
+#     '584 i',
+
+#     # 'dir f',
+#     # '$ cd f',
+#     # '$ ls',
+#     # '111 file',
+#     # '222 file',
+#     # '$ cd ..',
+
+#     '$ cd ..',
+#     '$ cd ..',
+#     '$ cd d',
+#     '$ ls',
+#     '4060174 j',
+#     '8033020 d.log',
+#     '5626152 d.ext',
+#     '7214296 k',
+# ]
+
+dirs = {}
+def calc_size(dir_name, cmds):
+    dir_stack = [dir_name]
+    
+    size = 0
+    for i in range(len(cmds)):
+        cmd = cmds[i]
+
+        if cmd == '$ cd ..':
+            dir_stack.pop()
+            if not dir_stack:
+                break
+        elif cmd.startswith('$ cd'):
+            child_name = cmd.split()[-1]
+            dir_stack.append(child_name)
+            calc_size(dir_stack[-1], cmds[i+1:])
+        
+        if cmd[0].isnumeric():
+            size += int(cmd.split()[0])
+
+    while dir_name in dirs:
+        # TODO - Very hacky, assumes directories with duplicate names have to have different sizes
+        #       But I've run out of time now.
+        if dirs[dir_name] != size:
+            dir_name += '0'
+        else:
+            break
+    dirs[dir_name] = size
+    return size
+
+
+def solve1(cmds):
+    calc_size('/', cmds[1:])
+    print(dirs)
+    size = 0
+    for s in dirs.values():
+        if s <= 100000:
+            size += s
+    return size
+
+ans = solve1(commands)
+print(ans)
+# Answer was 1749646
+
+
+def solve2():
+    space_needed = dirs['/'] - 40000000
+    min_dir = dirs['/']
+    for d in dirs.values():
+        if d < min_dir and d > space_needed:
+            min_dir = d
+    return min_dir
+
+ans = solve2()
+print(ans)
+# Answer was 1498966
