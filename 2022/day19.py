d70532228652a9185ee0f9e2153d00826ae083d2
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-19 15:01:12 +0530

    Added 2022 day 19

--- 2022/day19.py
+++ 2022/day19.py
@@ -1,12 +1,7 @@
-from time import time
+from collections import deque
 
 with open("inputs/input-19.txt", "r") as file:
     lines = list(map(lambda x: x.strip(), file.readlines()))
-
-lines = [
-    "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
-    "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
-]
 
 
 def get_blueprint_costs(blueprint):
@@ -19,125 +14,60 @@ def get_blueprint_costs(blueprint):
     return costs
 
 
-def get_max_geodes(
-    costs,
-    n_ore=0,
-    n_clay=0,
-    n_obsidian=0,
-    n_geodes=0,
-    n_ore_robots=1,
-    n_clay_robots=0,
-    n_obsidian_robots=0,
-    n_geode_robots=0,
-    minutes_left=24,
-    cache={}
-):
-    if minutes_left <= 1:
-        return n_geodes
-    # if (n_ore, n_clay, n_obsidian, n_geodes, minutes_left) in cache:
-    #     return n_geodes + cache[(n_ore, n_clay, n_obsidian, n_geodes, minutes_left)]
-
-    results = [
-        get_max_geodes(
-            costs,
-            n_ore + n_ore_robots,
-            n_clay + n_clay_robots,
-            n_obsidian + n_obsidian_robots,
-            n_geodes + n_geode_robots,
-            n_ore_robots,
-            n_clay_robots,
-            n_obsidian_robots,
-            n_geode_robots,
-            minutes_left - 1,
-            cache
-        )
-    ]
-
-    if n_ore >= costs["ore"][0]:
-        # We can build an ore robot
-        results.append(
-            get_max_geodes(
-                costs,
-                n_ore - costs['ore'][0] + n_ore_robots,
-                n_clay + n_clay_robots,
-                n_obsidian + n_obsidian_robots,
-                n_geodes + n_geode_robots,
-                n_ore_robots + 1,
-                n_clay_robots,
-                n_obsidian_robots,
-                n_geode_robots,
-                minutes_left - 1,
-                cache
-            )
-        )
-
-    if n_ore >= costs["clay"][0]:
-        # We can build a clay robot
-        results.append(
-            get_max_geodes(
-                costs,
-                n_ore - costs['clay'][0] + n_ore_robots,
-                n_clay + n_clay_robots,
-                n_obsidian + n_obsidian_robots,
-                n_geodes + n_geode_robots,
-                n_ore_robots,
-                n_clay_robots + 1,
-                n_obsidian_robots,
-                n_geode_robots,
-                minutes_left - 1,
-                cache
-            )
-        )
-
-    if n_ore >= costs["obsidian"][0] and n_clay >= costs["obsidian"][1]:
-        # We can build an obsidian robot
-        results.append(
-            get_max_geodes(
-                costs,
-                n_ore - costs['obsidian'][0] + n_ore_robots,
-                n_clay - costs['obsidian'][1] + n_clay_robots,
-                n_obsidian + n_obsidian_robots,
-                n_geodes + n_geode_robots,
-                n_ore_robots,
-                n_clay_robots,
-                n_obsidian_robots + 1,
-                n_geode_robots,
-                minutes_left - 1,
-                cache
-            )
-        )
-
-    if n_ore >= costs["geode"][0] and n_obsidian >= costs["geode"][2]:
-        # We can build a geode robot
-        results.append(
-            get_max_geodes(
-                costs,
-                n_ore - costs['geode'][0] + n_ore_robots,
-                n_clay + n_clay_robots,
-                n_obsidian - costs['geode'][2] + n_obsidian_robots,
-                n_geodes + n_geode_robots,
-                n_ore_robots,
-                n_clay_robots,
-                n_obsidian_robots,
-                n_geode_robots + 1,
-                minutes_left - 1,
-                cache
-            )
-        )
+def get_max_geodes(costs, time_left):
+    best = 0
+    state = (0, 0, 0, 0, 1, 0, 0, 0, time_left)
+    max_ore = max(costs[x][0] for x in costs)
+    max_clay = costs['obsidian'][1]
+    max_obs = costs['geode'][2]
+    q = deque()
+    q.append(state)
+    seen = set()
+    while q:
+        s = q.popleft()
+        n_ore, n_clay, n_obs, n_geo, n_ore_r, n_clay_r, n_obs_r, n_geo_r, t = s
+        if best > n_geo + t * n_geo_r + t * (t - 1) / 2:
+            continue
+        best = max(best, n_geo)
+        if t == 0:
+            continue
+            
+        if s in seen:
+            continue
+        seen.add(s)
+        
+        if n_ore >= costs['geode'][0] and n_obs >= costs['geode'][2]:
+            q.append((n_ore - costs['geode'][0] + n_ore_r, n_clay + n_clay_r, n_obs - costs['geode'][2] + n_obs_r, n_geo + n_geo_r,
+                      n_ore_r, n_clay_r, n_obs_r, n_geo_r + 1, t - 1))
+        else:
+            q.append((n_ore + n_ore_r, n_clay + n_clay_r, n_obs + n_obs_r, n_geo + n_geo_r, n_ore_r, n_clay_r, n_obs_r,
+                      n_geo_r, t - 1))
+            if n_ore >= costs['ore'][0] and n_ore_r <= max_ore and n_ore < max_ore * t:
+                q.append((n_ore - costs['ore'][0] + n_ore_r, n_clay + n_clay_r, n_obs + n_obs_r, n_geo + n_geo_r, n_ore_r + 1, n_clay_r, n_obs_r, n_geo_r, t-1))
+            if n_ore >= costs['clay'][0] and n_clay_r <= max_clay and n_clay < max_clay * t:
+                q.append((n_ore - costs['clay'][0] + n_ore_r, n_clay + n_clay_r, n_obs + n_obs_r, n_geo + n_geo_r,
+                          n_ore_r, n_clay_r + 1, n_obs_r, n_geo_r, t - 1))
+            if n_ore >= costs['obsidian'][0] and n_clay >= costs['obsidian'][1] and n_obs_r <= max_obs and n_obs < max_obs * t:
+                q.append((n_ore - costs['obsidian'][0] + n_ore_r, n_clay - costs['obsidian'][1] + n_clay_r, n_obs + n_obs_r, n_geo + n_geo_r,
+                          n_ore_r, n_clay_r, n_obs_r + 1, n_geo_r, t - 1))
     
-    ans = max(results)
-    # cache[(n_ore, n_clay, n_obsidian, n_geodes, minutes_left)] = ans
-    return ans
+    return best
 
-tic = time()
-print(get_max_geodes(get_blueprint_costs(lines[0]), minutes_left=25))
-toc = time()
-print(f'Took {toc-tic}s')
 
 def solve1(blueprints):
     score = 0
     for i, bluep in enumerate(blueprints):
-        score += (i + 1) * get_max_geodes(get_blueprint_costs(bluep), minutes_left=25)
+        score += (i + 1) * get_max_geodes(get_blueprint_costs(bluep), 24)
     return score
 
-# print(solve1(lines))
+print(solve1(lines))
+
+
+def solve2(blueprints):
+    score = 1
+    for b in blueprints:
+        score *= get_max_geodes(get_blueprint_costs(b), 32)
+    return score
+
+
+print(solve2(lines[:3]))
