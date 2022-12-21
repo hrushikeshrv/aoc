81c52d11f4f46fac4f84db81b5699c828d1c246a
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-12-21 11:45:00 +0530

    Added 2022 day 21

--- 2022/day21.py
+++ 2022/day21.py
@@ -1,30 +1,10 @@
 with open('inputs/input-21.txt', 'r') as file:
     lines = list(map(lambda x: x.strip(), file.readlines()))
 
-# lines = [
-#     'root: pppw + sjmn',
-#     'dbpl: 5',
-#     'cczh: sllz + lgvd',
-#     'zczc: 2',
-#     'ptdq: humn - dvpt',
-#     'dvpt: 3',
-#     'lfqf: 4',
-#     'humn: 5',
-#     'ljgn: 2',
-#     'sjmn: drzm * dbpl',
-#     'sllz: 4',
-#     'pppw: cczh / lfqf',
-#     'lgvd: ljgn * ptdq',
-#     'drzm: hmdt - zczc',
-#     'hmdt: 32',
-# ]
-
 monkeys = {}
 for line in lines:
     line = line.split(':')
     monkeys[line[0].strip()] = int(line[1].strip()) if line[1].strip().isnumeric() else line[1].strip().split()
-
-# print(monkeys)
 
 
 def get_monkey_number(monkey, monkey_jobs):
@@ -40,3 +20,18 @@ def get_monkey_number(monkey, monkey_jobs):
     return int(ops[op](get_monkey_number(m1, monkeys), get_monkey_number(m2, monkeys)))
 
 print(get_monkey_number('root', monkeys))
+
+
+def get_human_expression(monkey, monkey_jobs):
+    if monkey == 'humn':
+        return 'H'
+    if monkey == 'root':
+        m1, _, m2 = monkey_jobs[monkey]
+        return f'{get_human_expression(m1, monkey_jobs)} = {get_human_expression(m2, monkey_jobs)}'
+    if isinstance(monkey_jobs[monkey], int):
+        return monkey_jobs[monkey]
+    m1, op, m2 = monkey_jobs[monkey]
+    return f'({get_human_expression(m1, monkey_jobs)} {op} {get_human_expression(m2, monkey_jobs)})'
+
+print(get_human_expression('root', monkeys))
+# Pasted this expression into this expression solver to get the answer - https://www.mathpapa.com/equation-solver/
