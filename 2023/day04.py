4f15218936c71cb963b028255ccf32f006d4c35e
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2023-12-04 10:13:55 +0530

    Added 2023 day 4 skeleton

---
+++ 2023/day04.py
@@ -0,0 +1,10 @@
+with open('inputs/input-04.txt') as file:
+    lines = list(map(lambda x: x.strip(), file.readlines()))
+
+
+def solve1():
+    pass
+
+
+if __name__ == '__main__':
+    print(solve1())
